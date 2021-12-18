3ac23cf4b89629471bc91aef378773f57f57078f
Fadil <86321131+scoding2@users.noreply.github.com>
2021-12-18 15:13:07 -0800

    Add files via upload

---
+++ dashboard.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import streamlit as st
+import yfinance as yf
+
+
+st.title('Stock & Crypto Dashboard')
+
+# Stock and Crypto Signs
+tickers = ('TSLA', 'AAPL', 'MSFT', 'BTC-USD', 'ETH-USD', 'LCID', 'BB')
+crypto = ('BTC-USD', 'ETH-USD')
+
+# Dropdown Menu
+dropdown = st.multiselect('Pick your Stock asset', tickers)
+dropdown_crypto = st.multiselect('Pick your Crypto asset', crypto)
+
+# Dropdown Menu(Dates)
+start = st.date_input('Start', value = pd.to_datetime('2021-01-01'))
+end = st.date_input('End', value = pd.to_datetime('today'))
+
+
+if len(dropdown_crypto) > 0:
+    df = yf.download(dropdown_crypto,start,end)['Adj Close']
+    st.header('Return of {}'.format(dropdown_crypto))
+    st.line_chart(df)
+
+if len(dropdown) > 0:
+    df = yf.download(dropdown,start,end)['Adj Close']
+    st.header('Return of {}'.format(dropdown))
+    st.line_chart(df)
