1c3f37c4a9a5327790c1afb76fff5f073da8a547
Fadil <86321131+scoding2@users.noreply.github.com>
2022-02-19 10:18:51 -0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -6,16 +6,16 @@ import yfinance as yf
 st.title('Stock & Crypto Dashboard')
 
 # Stock and Crypto Signs
-tickers = ('TSLA', 'AAPL', 'MSFT', 'LCID', 'BB', 'FB')
+tickers = ('TSLA', 'GOOG', 'AAPL', 'MSFT', 'LCID', 'BB', 'FB', 'WMT', 'AMZN')
 crypto = ('BTC-USD', 'ETH-USD', 'DOGE-USD', 'SHIB-USD')
 
 # Dropdown Menu
-dropdown = st.multiselect('Pick your Stock asset', tickers)
-dropdown_crypto = st.multiselect('Pick your Crypto asset', crypto)
+dropdown = st.multiselect('Pick your Stock Asset', tickers)
+dropdown_crypto = st.multiselect('Pick your Crypto Asset', crypto)
 
 # Dropdown Menu(Dates)
-start = st.date_input('Start', value = pd.to_datetime('2021-01-01'))
-end = st.date_input('End', value = pd.to_datetime('today'))
+start = st.date_input('Start Date', value = pd.to_datetime('2021-01-01'))
+end = st.date_input('End Date', value = pd.to_datetime('today'))
 
 
 if len(dropdown_crypto) > 0:
