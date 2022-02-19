ff0e336a52918d5ded122240a6623d5eb5bac130
Fadil <86321131+scoding2@users.noreply.github.com>
2022-02-19 10:21:43 -0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -7,7 +7,7 @@ st.title('Stock & Crypto Dashboard')
 
 # Stock and Crypto Signs
 tickers = ('TSLA', 'GOOG', 'AAPL', 'MSFT', 'LCID', 'BB', 'FB', 'WMT', 'AMZN')
-crypto = ('BTC-USD', 'ETH-USD', 'DOGE-USD', 'SHIB-USD', 'ADA-USD')
+crypto = ('BTC-USD', 'ETH-USD', 'DOGE-USD', 'SHIB-USD', 'ADA-USD', 'LTC-USD')
 
 # Dropdown Menu
 dropdown = st.multiselect('Pick your Stock Asset', tickers)
